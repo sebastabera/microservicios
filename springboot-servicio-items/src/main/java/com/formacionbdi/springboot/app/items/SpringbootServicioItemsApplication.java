9ac53ea6fb7933c758f09c703f67c8cce78bbed6
Luis Sebastian Tabera Quintero <lstabera@personalsoft.com.co>
2019-10-02 13:58:02 -0500

    integración con eureka

--- springboot-servicio-items/src/main/java/com/formacionbdi/springboot/app/items/SpringbootServicioItemsApplication.java
+++ springboot-servicio-items/src/main/java/com/formacionbdi/springboot/app/items/SpringbootServicioItemsApplication.java
@@ -2,10 +2,23 @@ package com.formacionbdi.springboot.app.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="servicio-productos")
+/**
+ * la anotacion @EnableEurekaClient no es obligatoria agregarla ya que al tenerla en el pom
+ * se tiene automaticamente
+ * 
+ * @author lstabera
+ *
+ */
+@EnableEurekaClient
+/**
+ * se quita la anotacion @RibbonClient ya que esto viene configurado en el servidor de eureka
+ * @author lstabera
+ *
+ */
+//@RibbonClient(name="servicio-productos")
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemsApplication {
--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,7 +2,16 @@ package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * la anotacion @EnableEurekaClient no es obligatoria agregarla ya que al tenerla en el pom
+ * se tiene automaticamente
+ * 
+ * @author lstabera
+ *
+ */
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
 
