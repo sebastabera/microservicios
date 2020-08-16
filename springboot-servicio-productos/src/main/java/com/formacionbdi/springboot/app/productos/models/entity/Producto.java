51db82ee4281511eb5559edb5ac37b80874dde78
Luis Sebastian Tabera Quintero <lstabera@personalsoft.com.co>
2020-08-16 17:33:34 -0500

    servidor de configuracion

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
@@ -18,7 +18,7 @@ import javax.persistence.Transient;
 public class Producto implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
 	private double precio;
